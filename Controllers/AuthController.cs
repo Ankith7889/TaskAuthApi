025f2ba4d29551bb7edeb9de54e21861e15ad7bb
Ankith7889 <ankithkullal7889@gmail.com>
2025-07-07 11:11:57 +0530

    created dtos and authcontroller for login and register

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskAuthApi.Dtos;
+using TaskAuthApi.Models;
+
+namespace TaskAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var user = new AppUser { UserName = dto.UserName, Email = dto.EmailAddress };
+            var result = await _userManager.CreateAsync(user,dto.Password);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+            return Ok("user registered succesfully");
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(dto.UserName, dto.Password, false, false);
+
+            if (!result.Succeeded)
+                return Unauthorized("Invalid login");
+
+            return Ok("Logged in successfully");
+        }
+    }
+}
